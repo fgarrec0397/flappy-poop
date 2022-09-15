0b22da19a1a5bda0bb86dce853f2b8af8bee7429
fga97 <fgarrec0397@gmail.com>
2022-09-15 14:29:46 -0400

    createWidget description

--- app/src/App/Widgets/_actions/utilities/createWidget.ts
+++ app/src/App/Widgets/_actions/utilities/createWidget.ts
@@ -3,6 +3,9 @@ import { forwardRef, ForwardRefRenderFunction } from "react";
 
 import { WidgetComponent, WidgetModule } from "../widgetsTypes";
 
+/**
+ * A function helping you creating a widget
+ */
 export default <PropsType, RefType = null>(widget: WidgetModule<PropsType, RefType>) => {
     const widgetModule: WidgetModule<PropsType, RefType> = clone(widget);
 
