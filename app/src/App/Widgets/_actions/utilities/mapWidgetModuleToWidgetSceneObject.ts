ddeeefe767b1b8ea0398489a806bcab25bee8c9f
fga97 <fgarrec0397@gmail.com>
2022-09-16 10:49:52 -0400

    fix hasRef

--- app/src/App/Widgets/_actions/utilities/mapWidgetModuleToWidgetSceneObject.ts
+++ app/src/App/Widgets/_actions/utilities/mapWidgetModuleToWidgetSceneObject.ts
@@ -4,6 +4,7 @@ export default (widget: WidgetModule) => {
     const widgetSceneObject: WidgetSceneObject = {
         id: "",
         component: widget.component,
+        hasRef: widget.hasRef,
         widgetDefinition: widget.widgetDefinition,
         editorOptions: widget.editorOptions,
     };
